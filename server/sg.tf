ff970610e35035d7e7120c53676d1adca6510447
Darren Adams <dadams@illinois.edu>
2022-09-30 05:23:24 -0500

    Create sg.tf

---
+++ server/sg.tf
@@ -0,0 +1,40 @@
+locals {
+  aws_sg_name = "${var.name}_${var.env}_allow_ssh_http"
+}
+
+resource "aws_security_group" "sg" {
+  name        = local.aws_sg_name
+  description = "Allow ssh http inbound traffic"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = local.aws_sg_name
+    Session = "Terraform"
+  }
+}
