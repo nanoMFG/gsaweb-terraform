021b02657e91017aa649963a48768563c8efd1ce
Darren Adams <dadams@illinois.edu>
2022-10-13 08:13:52 -0500

    Update instance.tf

--- server/instance.tf
+++ server/instance.tf
@@ -1,23 +1,23 @@
-# resource "aws_instance" "web" {
-#   ami             = var.instance_ami
-#   instance_type   = var.instance_type
-#   key_name        = var.instance_key
-#   subnet_id       = aws_subnet.public_subnet.id
-#   security_groups = [aws_security_group.sg.id]
+resource "aws_instance" "web" {
+  ami             = var.instance_ami
+  instance_type   = var.instance_type
+  key_name        = module.key_pair.key_pair_name
+  subnet_id       = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
 
-#   user_data = <<-EOF
-#   #!/bin/bash
-#   echo "*** Installing apache2"
-#   sudo apt update -y
-#   sudo apt install apache2 -y
-#   echo "*** Completed Installing apache2"
-#   EOF
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
 
-#   tags = {
-#     Name = "${var.name}_${var.env}_web_instance"
-#   }
+  tags = {
+    Name = "${var.name}_${var.env}_web_instance"
+  }
 
-#   volume_tags = {
-#     Name = "${var.name}_${var.env}_web_instance"
-#   } 
-# }
+  volume_tags = {
+    Name = "${var.name}_${var.env}_web_instance"
+  } 
+}
