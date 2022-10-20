49f01bcc992f91cb0d3dcec16b29b2b0b43fc7db
Darren Adams <dadams@illinois.edu>
2022-10-20 09:07:52 -0500

    Update variables.tf

--- server/variables.tf
+++ server/variables.tf
@@ -1,3 +1,13 @@
+variable "name" {
+  description = "Project name"
+  type        = string
+  default = "gsaweb"
+}
+variable "env" {
+  description = "Project environment such as dev, staging, production"
+  type        = string
+}
+# DNS
 variable "create_dns_zone" {
   description = "If true, create new route53 zone, if false read existing route53 zone"
   type        = bool
@@ -8,15 +18,7 @@ variable "domain" {
   type        = string
   default = "127.0.0.1"
 }
-variable "name" {
-  description = "Project name"
-  type        = string
-  default = "gsaweb"
-}
-variable "env" {
-  description = "Project environment such as dev, staging, production"
-  type        = string
-}
+# VPC and Subnets
 variable "vpc_cidr" {
 default = "178.0.0.0/16"
 }
@@ -25,6 +27,10 @@ default = "us-east-2a"
 }
 variable "public_subnet_cidr" {
 default = "178.0.10.0/24"
+}
+variable "aws_instance" {
+  description = "If true, use plain aws_instance (EC2) resources directly."
+  default = true
 }
 variable "instance_type" {
 default = "t3-micro"
