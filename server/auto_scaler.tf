de1ca99087c0552585faa9a4b064a24f09a6bc42
Darren Adams <dadams@illinois.edu>
2022-10-20 09:40:45 -0500

    Update auto_scaler.tf

--- server/auto_scaler.tf
+++ server/auto_scaler.tf
@@ -1,9 +1,9 @@
 # resource "aws_launch_configuration" "terramino" {
 #   name_prefix     = "learn-terraform-aws-asg-"
 #   image_id        = data.aws_ami.amazon-linux.id
-#   instance_type   = "t2.micro"
+#   instance_type   = "t3.micro"
 #   user_data       = file("user-data.sh")
-#   security_groups = [aws_security_group.terramino_instance.id]
+#   security_groups = [aws_security_group.sg.id]
 
 #   lifecycle {
 #     create_before_destroy = true
