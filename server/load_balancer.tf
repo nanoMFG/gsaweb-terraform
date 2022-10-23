40b25c0079c68584a95aff8785464285a6d4e6bf
Darren Adams <dadams@illinois.edu>
2022-10-23 10:29:27 -0500

    Create load_balancer.tf

---
+++ server/load_balancer.tf
@@ -0,0 +1,52 @@
+resource "aws_lb_listener" "http_listner" {
+  count = var.aws_load_balancer ? 1 : 0
+  load_balancer_arn = aws_lb.load_balancer.arn
+
+  port = 80
+
+  protocol = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "instances" {
+  name     = "${var.name}_${var.env}_target_group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "instances" {
+  listener_arn = aws_lb_listener.http_listner.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.instances.arn
+  }
+}
