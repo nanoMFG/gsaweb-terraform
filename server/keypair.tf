2ed27ec3a8073a9db3f1f10d6787f4ad0a250103
Darren Adams <dadams@illinois.edu>
2023-05-19 16:54:05 -0500

    add local file for key

--- server/keypair.tf
+++ server/keypair.tf
@@ -6,3 +6,7 @@ resource "aws_key_pair" "generated_key" {
   key_name   = "generated-key"
   public_key = tls_private_key.key.public_key_openssh
 }
+resource "local_file" "private_key" {
+  sensitive_content = tls_private_key.key.private_key_pem
+  filename          = "${path.module}/private_key.pem"
+}
