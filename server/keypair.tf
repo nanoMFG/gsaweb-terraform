b643e3d28fc25bf9aaafa0854109d0c31bcc48be
Darren Adams <dadams@illinois.edu>
2023-05-19 16:56:58 -0500

    key fix

--- server/keypair.tf
+++ server/keypair.tf
@@ -6,7 +6,9 @@ resource "aws_key_pair" "generated_key" {
   key_name   = "generated-key"
   public_key = tls_private_key.key.public_key_openssh
 }
-resource "local_file" "private_key" {
-  sensitive_content = tls_private_key.key.private_key_pem
-  filename          = "${path.module}/private_key.pem"
+
+output "private_key_pem" {
+  description = "The private key in PEM format"
+  value       = tls_private_key.key.private_key_pem
+  sensitive   = true
 }
--- server/output.tf
+++ server/output.tf
@@ -2,8 +2,3 @@ output "instance_public_ip" {
   value       = aws_instance.web.public_ip
   description = "Public IP of the EC2 instance"
 }
-output "private_key_pem" {
-  description = "The private key in PEM format"
-  value       = tls_private_key.key.private_key_pem
-  sensitive   = true
-}
