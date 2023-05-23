0439389c22ff6b1ea7b29142d7f314b0a09702b5
Darren Adams <dadams@illinois.edu>
2023-05-23 08:53:42 -0500

    more fixes

--- server/route53.tf
+++ server/route53.tf
@@ -11,7 +11,7 @@ resource "aws_route53_record" "www" {
 
   alias {
     name                   = aws_elb.web-elb.dns_name
-    zone_id                = aws_elb.example.zone_id
+    zone_id                = aws_elb.web-elb.zone_id
     evaluate_target_health = true
   }
 
@@ -37,14 +37,23 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
-  zone_id = data.aws_route53_zone.existing_zone.zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = data.aws_route53_zone.existing_zone[0].zone_id
+  records = [each.value.record]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
 }
+
