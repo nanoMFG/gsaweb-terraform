8ffa1f9116a63c850785d8c905a295db4dc9d4e0
Darren Adams <dadams@illinois.edu>
2023-05-24 08:37:24 -0500

    fix

--- server/private_subnet.tf
+++ server/private_subnet.tf
@@ -16,7 +16,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.public_subnet[0].id
 
   tags = {
     Name = "${var.name}_${var.env}_nat"
