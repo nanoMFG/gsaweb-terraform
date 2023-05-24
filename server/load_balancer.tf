e27ae86f6618f9c2722c10acbdbffaecde3021ae
Darren Adams <dadams@illinois.edu>
2023-05-24 06:13:25 -0500

    switch to alb

--- server/load_balancer.tf
+++ server/load_balancer.tf
@@ -1,35 +1,108 @@
-resource "aws_elb" "webelb" {
-  name               = "${var.name}-${var.env}-elb"
+# resource "aws_elb" "webelb" {
+#   name               = "${var.name}-${var.env}-elb"
+#   subnets            = [aws_subnet.public_subnet.id]
+#   #availability_zones = var.availability_zones
+
+#   listener {
+#     instance_port      = 80
+#     instance_protocol  = "http"
+#     lb_port            = 80
+#     lb_protocol        = "http"
+#   }
+
+#   listener {
+#     instance_port      = 80
+#     instance_protocol  = "http"
+#     lb_port            = 443
+#     lb_protocol        = "https"
+#     ssl_certificate_id = aws_acm_certificate.cert.arn
+#   }
+
+#   instances = [aws_instance.web.id]
+
+#   health_check {
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 3
+#     target              = "HTTP:80/"
+#     interval            = 30
+#   }
+
+#   tags = {
+#     Name        = "${var.name}_${var.env}_elb"
+#     Environment = var.env
+#   }
+# }
+resource "aws_lb" "web" {
+  name               = "${var.name}-${var.env}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb_sg.id]
   subnets            = [aws_subnet.public_subnet.id]
-  #availability_zones = var.availability_zones
 
-  listener {
-    instance_port      = 80
-    instance_protocol  = "http"
-    lb_port            = 80
-    lb_protocol        = "http"
-  }
-
-  listener {
-    instance_port      = 80
-    instance_protocol  = "http"
-    lb_port            = 443
-    lb_protocol        = "https"
-    ssl_certificate_id = aws_acm_certificate.cert.arn
-  }
-
-  instances = [aws_instance.web.id]
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
-  }
+  enable_deletion_protection = false
 
   tags = {
-    Name        = "${var.name}_${var.env}_elb"
+    Name        = "${var.name}_${var.env}_alb"
     Environment = var.env
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.cert.arn
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
+
+resource "aws_lb_target_group" "web" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app_vpc.id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    protocol            = "HTTP"
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web" {
+  target_group_arn = aws_lb_target_group.web.arn
+  target_id        = aws_instance.web.id
+}
+
+resource "aws_security_group" "elb_sg" {
+  name        = "${var.name}_${var.env}_elb_sg"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "${var.name}_${var.env}_elb_sg"
   }
 }
