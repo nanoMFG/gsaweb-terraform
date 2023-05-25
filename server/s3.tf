4ddccc32b2a7489afe1d08a4ce409cb89ef9ec03
Darren Adams <dadams@illinois.edu>
2023-05-24 19:54:25 -0500

    fix

--- server/s3.tf
+++ server/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "ansible_bucket" {
   bucket = "${var.name}-${var.env}-ansilbe-bucket"
+  acl = "private"
   
   lifecycle {
     prevent_destroy = true
@@ -11,10 +12,10 @@ resource "aws_s3_bucket" "ansible_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "ansible_bucket_acl" {
-  bucket = aws_s3_bucket.ansible_bucket.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "ansible_bucket_acl" {
+#   bucket = aws_s3_bucket.ansible_bucket.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_versioning" "ansible_bucket_versioning" {
   bucket = aws_s3_bucket.ansible_bucket.id
