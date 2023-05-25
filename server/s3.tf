b2a5901dab0b917809611c4ab06f3c17651daf3c
Darren Adams <dadams@illinois.edu>
2023-05-25 08:31:42 -0500

    cleanup

--- server/s3.tf
+++ server/s3.tf
@@ -1,10 +1,9 @@
 resource "aws_s3_bucket" "ansible_bucket" {
   bucket = "${var.name}-${var.env}-ansilbe-bucket"
-#   acl = "private"
   
-#   lifecycle {
-#     prevent_destroy = true
-#   }
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = {
     Name        = "${var.name}-${var.env}-ansible-bucket"
--- server/variables.tf
+++ server/variables.tf
@@ -7,12 +7,6 @@ variable "env" {
   description = "Project environment such as dev, staging, production"
   type        = string
 }
-# DNS
-# variable "create_dns_zone" {
-#   description = "If true, create new route53 zone, if false read existing route53 zone"
-#   type        = bool
-#   default     = false
-# }
 variable "domain_name" {
   description = "Domain for website"
   type        = string
@@ -22,13 +16,6 @@ variable "domain_name" {
 variable "vpc_cidr" {
 default = "178.0.0.0/16"
 }
-# variable "subnet_zone" {
-# default = "us-east-2a"
-# }
-# variable "public_subnet_cidr" {
-#   description = "CIDR block for the public subnet"
-#   default = "178.0.10.0/24"
-# }
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet"
   default     = "178.0.10.0/24"
@@ -43,33 +30,14 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["178.0.20.0/24", "178.0.30.0/24"]
 }
-# variable "aws_instance" {
-#   description = "If true, use plain aws_instance (EC2) resources directly."
-#   type = bool
-#   default = true
-# }
-# variable "aws_auto_scaler" {
-#   description = "IF true, negates aws_instance and deploys auto scaler instead"
-#   type = bool
-#   default = false
-# }
 variable "instance_type" {
 default = "t3-micro"
 }
 variable "instance_ami" {
   default = "ami-097a2df4ac947655f"
 }
-# variable "aws_load_balancer" {
-#   default = false
-#   type = bool
-# }
 variable "route53_zone_id" {
   description = "The ID of the existing Route53 hosted zone"
   type        = string
   default     = "dummy"
 }
-# variable "use_existing_route53_zone" {
-#   description = "If true, use an existing Route53 zone for the environment, if false, do not use it"
-#   type        = bool
-#   default     = false
-# }
