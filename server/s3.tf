504ab7491f55ab121ba4aca5f75c1eb23d4cc0ca
Darren Adams <dadams@illinois.edu>
2023-05-24 19:41:47 -0500

    update to fix warning

--- server/s3.tf
+++ server/s3.tf
@@ -1,17 +1,19 @@
 resource "aws_s3_bucket" "ansible_bucket" {
   bucket = "${var.name}-${var.env}-ansilbe-bucket"
   acl    = "private"
-
-  versioning {
-    enabled = true
-  }
   
   lifecycle {
     prevent_destroy = true
   }
 
   tags = {
-    Name        = "${var.name}-${var.env}-ansilbe-bucket"
+    Name        = "${var.name}-${var.env}-ansible-bucket"
     Environment = "${var.env}"
   }
+}
+
+resource "aws_s3_bucket_versioning" "ansible_bucket_versioning" {
+  bucket = aws_s3_bucket.ansible_bucket.id
+  
+  status = "Suspended" // Or "Enalbled" 
 }
