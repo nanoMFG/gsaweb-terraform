f0b70f4e3e0ba8b730782e6c3fcc9e1f3f3a7c72
Darren Adams <dadams@illinois.edu>
2023-06-02 14:04:54 -0500

    format

--- cert/main.tf
+++ cert/main.tf
@@ -31,7 +31,7 @@ output "certificate_arn" {
   value = aws_acm_certificate.cert.arn
 }
 output "certificate_dvo" {
-    value = aws_acm_certificate.cert.domain_validation_options
+   value = aws_acm_certificate.cert.domain_validation_options
 }
 output "certificate_domain_name" {
   value = aws_acm_certificate.cert.domain_name
