063f61775eda880bf28293e3dd504f400eab14f8
Darren Adams <dadams@illinois.edu>
2023-06-02 11:45:52 -0500

    add cer module

---
+++ cert/main.tf
@@ -0,0 +1,32 @@
+# Requests a new wildcard SSL/TLS certificate from AWS Certificate Manager (ACM).
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${var.env}.${var.domain_name}"
+  validation_method = "DNS"
+
+  tags = {
+    Name        = "${var.name}_${var.env}_certificate"
+    Environment = var.env
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# Defines a variable to be used as the name in the resource tags
+variable "name" {
+  description = "Project name"
+  type        = string
+  default     = "gsaweb"
+}
+# Defines a variable to be used as the environment in the resource tags
+variable "env" {
+  description = "Project environment such as dev, qa or prod"
+  type        = string
+}
+variable "domain_name" {
+  description = "Domain name"
+  type        = string
+}
+output "certificate_arn" {
+  value = aws_acm_certificate.cert.arn
+}
