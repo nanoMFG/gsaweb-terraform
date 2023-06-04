1df30f6a509801f6f147c6903d40da555b19cead
Darren Adams <dadams@illinois.edu>
2023-06-04 08:32:57 -0500

    remove dup. outputs

--- network/cert.tf
+++ network/cert.tf
@@ -12,13 +12,3 @@ resource "aws_acm_certificate" "cert" {
     create_before_destroy = true
   }
 }
-
-output "certificate_arn" {
-  value = aws_acm_certificate.cert.arn
-}
-output "certificate_dvo" {
-   value = aws_acm_certificate.cert.domain_validation_options
-}
-output "certificate_domain_name" {
-  value = aws_acm_certificate.cert.domain_name
-}
