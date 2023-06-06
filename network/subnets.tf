fa164f49b77cf994b270f1d0ecc544d85415ac31
Darren Adams <dadams@illinois.edu>
2023-06-06 09:04:48 -0500

    variable tweak

--- network/subnets.tf
+++ network/subnets.tf
@@ -1,10 +1,13 @@
 data "aws_availability_zones" "available" {}
+locals {
+  alb_subnet_count = min(length(var.public_subnet_cidrs), length(data.aws_availability_zones.available.names))
+}
 # This resource block is responsible for creating public subnets within the specified VPC. 
 # The number of subnets created is determined by the length of the 'availability_zones' variable. 
 # Each subnet is associated with a unique CIDR block and availability zone.
 # For instances launched in these subnets, 'map_public_ip_on_launch' attribute is set to 'true' to enable automatic public IP assignment. 
 resource "aws_subnet" "public_subnet" {
-  count             = length(var.public_subnet_cidrs)
+  count             = local.alb_subnet_count
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = element(var.public_subnet_cidrs, count.index)
   map_public_ip_on_launch = true
@@ -18,7 +21,7 @@ resource "aws_subnet" "public_subnet" {
 # The count, determined by the length of the 'availability_zones' variable, corresponds to the number of subnets. 
 # It creates a route table association for each subnet, allowing to define routes for outbound traffic from the subnets.
 resource "aws_route_table_association" "public_rt_asso" {
-  count             = length(var.public_subnet_cidrs)
+  count             = local.alb_subnet_count
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public_rt.id
 }
